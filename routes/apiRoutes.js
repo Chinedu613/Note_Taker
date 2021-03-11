f89ef81c86b3214e1eff01c01b0ed92498b3782e
Latte <cchukwuani@gmail.com>
2021-03-11 01:07:38 -0500

    can post and get data

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,46 +6,48 @@ const path = require('path');
 
 
  module.exports = app => {
+    
+    const notesPath = path.join(__dirname,'../db/db.json')
 
-    //let notelist = fs.readFile(path.join(__dirname, "/db/db.json"));
-
+    // Builds route to get notes
     app.get('/api/notes', (req, res) => {
-
-        fs.readFile(__dirname + '/db/db.json', (err, notes) => {
+        
+        fs.readFile(notesPath, (err, notesJSON) => {
             if (err) {
                 console.log('Failed:', err)
                 return
             }
-            JSON.parse(notes);
+            console.log('API*********', notesJSON)
+            let notes = JSON.parse(notesJSON);
+            return res.json(notes);
         })
     });
     
- 
-
-
-
 
 
     app.post('/api/notes', (req, res) => {
         let newNote = req.body;
-        notes.push(newNote);
+        
+        console.log('line 30---------', newNote)
+        //let notesData = JSON.parse(fs.readFile(notesPath, 'utf8'));
+        //reads the JSON file and returns the notes data 
+        fs.readFile(notesPath,(err, notesJSON) => {
+            if(err) {
+                throw 'File Not Read:', err;
+            }
+            let notesData = JSON.parse(notesJSON)
+            console.log('line 37----------', notesData);
+        
+        
+            notesData.push(newNote);
+            console.log('line 41----------', notesData);
+            //Addd the new note to the JSON file
+            fs.writeFile(notesPath, JSON.stringify(notesData), err  => {
+                    if(err) {
+                throw "File Not written", err;
+                } 
+                return res.json(notesData)
+            });
+        });
     }); 
-
- }
- /* module.exports = app => {
-
-fs.writeFile
-
-    // Grabbing Notes from db.json
-app.get('/api/notes', (req, res) => {fs.readFile('../db/db.json', 'utf8', (err, notesJsonString) => {
-    if(err){
-        console.log("File read failed:", err)
-        return
-    } try{
-        const notesData = JSON.parse(notesJsonString)
-        console.log("Notes Title:", notesData)
-    }
-    catch(err){
-        console.log('Error parsing', notesJsonString)
-    }
-});*/
+}
